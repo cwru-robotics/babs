ce749cdf58eef05e62e645220298ea41de222ae0
nrgsy <tpech46@gmail.com>
2016-08-26 20:53:56 -0400

    changed dimensions

--- packages/high_level_control/sentry/babs_nav/src/navigation_goals.cpp
+++ packages/high_level_control/sentry/babs_nav/src/navigation_goals.cpp
@@ -1,11 +1,13 @@
 #include <ros/ros.h>
 #include <move_base_msgs/MoveBaseAction.h>
 #include <actionlib/client/simple_action_client.h>
+#include <geometry_msgs/PointStamped.h>
 
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 
-int main(int argc, char** argv){
-  ros::init(argc, argv, "simple_navigation_goals");
+void pointClickCallback(const geometry_msgs::PointStamped& pointStamped) {
+
+  ROS_INFO("Recieved point-click goal");
 
   //tell the action client that we want to spin a thread by default
   MoveBaseClient ac("move_base", true);
@@ -18,32 +20,33 @@ int main(int argc, char** argv){
   move_base_msgs::MoveBaseGoal goal;
 
   //we'll send a goal to the robot to move 1 meter forward
-  goal.target_pose.header.frame_id = "base_link";
+  goal.target_pose.header.frame_id = "map";
 
   goal.target_pose.header.stamp = ros::Time::now();
-  goal.target_pose.pose.position.x = 30.0;
-  goal.target_pose.pose.position.y = 0.0;
+
+  goal.target_pose.pose.position.x = pointStamped.point.x;
+  goal.target_pose.pose.position.y = pointStamped.point.y;
+
+
   goal.target_pose.pose.orientation.w = 1.0;
-  ROS_INFO("Sending goal");
+  ROS_INFO("Sending point-click goal");
   ac.sendGoal(goal);
   ac.waitForResult();
   if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
-    ROS_INFO("GOAL 1 SUCCESSS!");
+    ROS_INFO("GOAL REACHED");
   else
-    ROS_INFO("GOAL 1 FAIL!");
+    ROS_WARN("GOAL FAILED!!!");
+}
 
+int main(int argc, char** argv){
 
-  // goal.target_pose.header.stamp = ros::Time::now();
-  // goal.target_pose.pose.position.x = 3.0;
-  // goal.target_pose.pose.position.y = 1.0;
-  // goal.target_pose.pose.orientation.w = 1.0;
-  // ROS_INFO("2Sending goal");
-  // ac.sendGoal(goal);
-  // ac.waitForResult();
-  // if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
-  //   ROS_INFO("GOAL 2 SUCCESSS!");
-  // else
-  //   ROS_INFO("GOAL 2 FAIL!");
+  ROS_INFO("first!");
 
+  ros::init(argc, argv, "simple_navigation_goals");
+
+  ros::NodeHandle n;
+  ros::Subscriber point_click_subscriber = n.subscribe("clicked_point", 1, pointClickCallback);
+
+  ros::spin();
   return 0;
 }
